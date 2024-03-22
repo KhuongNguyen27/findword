32437658a9fbc1061141c6f3b72c28bc1bb4a9e6
tamnguyen <tamnguyen01>
2024-03-22 14:14:04 +0700

    change type db

--- Modules/Employee/database/migrations/2023_12_23_074412_create_jobs_table.php
+++ Modules/Employee/database/migrations/2023_12_23_074412_create_jobs_table.php
@@ -34,8 +34,8 @@ return new class extends Migration
             $table->string('end_day')->nullable();
             $table->string('start_hour')->nullable();
             $table->string('end_hour')->nullable();
-            $table->string('description')->nullable();
-            $table->string('requirements')->nullable();
+            $table->text('description')->nullable();
+            $table->text('requirements')->nullable();
             $table->timestamps();
         });
     }
