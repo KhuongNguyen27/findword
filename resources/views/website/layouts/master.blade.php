180d8533413d1c9e7975a746dc056a9c513930cd
taifamfc <taitam8590@gmail.com>
2024-03-24 16:39:50 +0700

    fix-4

--- resources/views/website/layouts/master.blade.php
+++ resources/views/website/layouts/master.blade.php
@@ -6,7 +6,7 @@
     <title>@yield('title')</title>
     <!-- Stylesheets -->
     <link href="{{ asset('website-assets/css/bootstrap.css')}}" rel="stylesheet">
-
+    <link href="{{ asset('website-assets/css/jquery-ui.min.css')}}" rel="stylesheet">
     <link href="{{ asset('website-assets/css/style.css')}}" rel="stylesheet">
     <link href="{{ asset('website-assets/css/custom.css')}}" rel="stylesheet">
     <link rel="shortcut icon" href="{{ asset('website-assets/images/favicon.png')}}" type="image/x-icon">
@@ -14,7 +14,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <link href="{{ asset('website-assets/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
-
+    <script src="{{ asset('website-assets/js/jquery.js')}}"></script>
+    <script src="{{ asset('website-assets/js/jquery-ui.min.js')}}"></script>
+    <link rel="stylesheet" href="https://static.topcv.vn/v4/css/components/desktop/welcome.min.f52a5c4bfa977462.css">
 </head>
 
 <body data-anm=".anm">
@@ -25,7 +27,10 @@
         @yield('content')
         @include('website.includes.footer')
     </div><!-- End Page Wrapper -->
-    <script src="{{ asset('website-assets/js/jquery.js')}}"></script>
+    <script>
+    // Resolve name collision between jQuery UI and Twitter Bootstrap
+    $.widget.bridge('uitooltip', $.ui.tooltip);
+    </script>
     <script src="{{ asset('website-assets/js/popper.min.js')}}"></script>
     <script src="{{ asset('website-assets/js/chosen.min.js')}}"></script>
     <script src="{{ asset('website-assets/js/bootstrap.min.js')}}"></script>
@@ -42,7 +47,7 @@
     <script src="{{ asset('website-assets/js/wow.js')}}"></script>
     <script src="{{ asset('website-assets/js/script.js')}}"></script>
     <script src="{{ asset('website-assets/js/repeater.js')}}"></script>
-    <script src="{{ asset('website-assets/js/custom.js')}}"></script>
+    <script src="{{ asset('website-assets/js/custom.js?t='.time())}}"></script>
     <script>
     $(document).ready(function() {
         $('.bookmark-btn').on('click', function(e) {
