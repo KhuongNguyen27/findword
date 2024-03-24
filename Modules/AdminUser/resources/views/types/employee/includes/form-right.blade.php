8b9f41b15ca8b17aaf2667f809b4d264ed6a6967
tamnguyen <tamnguyen01>
2024-03-24 22:44:19 +0700

    fix employee admin

--- Modules/AdminUser/resources/views/types/employee/includes/form-right.blade.php
+++ Modules/AdminUser/resources/views/types/employee/includes/form-right.blade.php
@@ -3,8 +3,10 @@
         <div class="mb-4">
             <label class="mb-3">{{ __('verify') }}</label>
             <select type="text" class="form-control" name="verify">
-                <option value="0" @selected($item->verify == 0)>{{ __('inconfirm') }}</option>
-                <option value="1" @selected($item->verify == 1)>{{ __('confirm') }}</option>
+                <option value="0" {{ isset($item) && $item->verify == 0 ? 'selected' : '' }}>{{ __('inconfirm') }}
+                </option>
+                <option value="1" {{ isset($item) && $item->verify == 1 ? 'selected' : '' }}>{{ __('confirm') }}
+                </option>
             </select>
             <x-admintheme::form-input-error field="verify" />
         </div>
