a9c6d50ba409143f5932a2288784872fbb59b0ff
hoanglongtpt <hoanglongtpt1999@gmail.com>
2024-03-26 16:16:21 +0700

    query-dashboard-admin

--- Modules/AdminHome/app/Http/Controllers/AdminHomeController.php
+++ Modules/AdminHome/app/Http/Controllers/AdminHomeController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Carbon\Carbon;
+use App\Models\User;
 
 class AdminHomeController extends Controller
 {
@@ -14,7 +16,62 @@ class AdminHomeController extends Controller
      */
     public function index()
     {
-        return view('adminhome::index');
+        $today = Carbon::now()->format('Y-m-d');
+        //employee
+            $total_employeer_create_today = User::where('type','employee')->whereDate('created_at',$today)->count();
+            $total_employeer = User::where('type','employee')->count();
+            // Số lượng tài khoản NTD tồn tại trong website
+            $total_employee_in_website = 0;
+            //số lượt NTD truy cập website
+            $total_employee_access_website = 0;
+            //số lượng tin đăng tuyển dụng
+            $total_jobs = 0;
+
+        //staff
+            $total_staff = User::where('type','staff')->count();
+            // Số lượng tài khoản UV tồn tại trong website
+            $total_staff_in_website = 0;
+            //Số lượt tài khoản UV truy cập website
+            $total_staff_access_website = 0;
+            $total_staff_create_today = User::where('type','staff')->whereDate('created_at',$today)->count();
+            
+        //query tổng:
+            //Số lần xem bài tin tuyển dụng
+            $total_view_jobs = 0;
+            // số lượng tin tuyển dụng có lượt nộp hồ sơ
+            $total_job_has_aplicaton = 0;
+            //Tỉ lệ giữ chân tài khoản NTD 
+            $retention_rate_employee = 0;
+            //Tỉ lệ giữ chân tài khoản UV
+            $retention_rate_staff = 0;
+            //Tỉ lệ rời bỏ của NTD
+            $churn_rate_employee = 0;
+            //Tỉ lệ rời bỏ của UV
+            $churn_rate_staff = 0;
+            //Tỉ lệ Tin tuyển dụng có hồ sơ ứng tuyển
+            $retention_rate_jobs_has_cv_aplication = 0;
+            //Tir lệ Nguồn truy cập website
+            $retention_rate_access_source_website = 0;
+        $param = [
+            'total_employeer_create_today' => $total_employeer_create_today,
+            'total_employeer' => $total_employeer,
+            'total_staff' => $total_staff,
+            'total_staff_create_today' => $total_staff_create_today,
+            'total_employee_in_website' => $total_employee_in_website,
+            'total_employee_access_website' => $total_employee_access_website,
+            'total_jobs' => $total_jobs,
+            'total_staff_in_website' => $total_staff_in_website,
+            'total_staff_access_website' => $total_staff_access_website,
+            'total_view_jobs' => $total_view_jobs,
+            'total_job_has_aplicaton' => $total_job_has_aplicaton,
+            'retention_rate_employee' => $retention_rate_employee,
+            'retention_rate_staff' => $retention_rate_staff,
+            'churn_rate_employee' => $churn_rate_employee,
+            'churn_rate_staff' => $churn_rate_staff,
+            'retention_rate_jobs_has_cv_aplication' => $retention_rate_jobs_has_cv_aplication,
+            'retention_rate_access_source_website' => $retention_rate_access_source_website
+        ];
+        return view('adminhome::index',$param);
     }
 
     /**
@@ -65,3 +122,6 @@ class AdminHomeController extends Controller
         //
     }
 }
+
+
+
