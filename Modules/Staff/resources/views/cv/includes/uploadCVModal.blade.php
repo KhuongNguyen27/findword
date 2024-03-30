345799caf4c86685b5678528b77b023ac0323c5b
tamnguyen <tamnguyen01>
2024-03-30 22:49:00 +0700

    upload cv success

--- Modules/Staff/resources/views/cv/includes/uploadCVModal.blade.php
+++ Modules/Staff/resources/views/cv/includes/uploadCVModal.blade.php
@@ -1,29 +1,31 @@
 <!-- Modal -->
 <div class="modal fade" id="uploadCVModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-        <!-- <form action="" enctype="multipart/form-data"> -->
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Tải CV</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="mb-3">
-                    <label for="exampleFormControlInput1" class="form-label">Tên CV</label>
-                    <input type="email" class="form-control" id="exampleFormControlInput1"
-                        placeholder="name@example.com">
+        <form action="{{ route('staff.uploadCV') }}" enctype="multipart/form-data" method="POST">
+            @csrf
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Tải CV</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="mb-3">
-                    <label for="exampleFormControlTextarea1" class="form-label">File</label>
-                    <input type="file" class="form-control" id="fileInput" onChange="validateFile()">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Tên CV</label>
+                        <input type="text" name="cv_file" class="form-control" id="exampleFormControlInput1"
+                            placeholder="Tên CV đưa lên" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1" class="form-label">File</label>
+                        <input name="file" type="file" class="form-control" id="fileInput" onChange="validateFile()"
+                            required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
                 </div>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-        </div>
-        <!-- </form> -->
+        </form>
     </div>
 </div>
 <script>
