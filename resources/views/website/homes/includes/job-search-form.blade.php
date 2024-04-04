ebac5005fe3ecf83b1af159021430fe085297f4d
nguyen phu <nguyenphu.126@gmail.com>
2024-04-04 11:12:05 +0700

    vnjob-internationnal-select-option-hidden

--- resources/views/website/homes/includes/job-search-form.blade.php
+++ resources/views/website/homes/includes/job-search-form.blade.php
@@ -6,9 +6,12 @@
                     <span class="icon flaticon-map-locator"></span>
                     <select name="province_id" class="form-select chosen-select">
                         <option value="">Tất cả địa điểm</option>
-                        <option value="quoc_te">Quốc tế</option>
+                        @if (!request()->routeIs('jobs.vnjobs'))
+                            <option value="quoc_te">Quốc tế</option>
+                        @endif
                         @foreach ($provinces as $province)
-                            <option @selected( $province->id == request()->province_id ) value="{{ $province->id }}">{{ $province->name }}</option>
+                            <option @selected($province->id == request()->province_id) value="{{ $province->id }}">{{ $province->name }}
+                            </option>
                         @endforeach
                     </select>
                 </div>
@@ -16,38 +19,41 @@
                 <div></div>
             @endif
             <!-- Form Group -->
-            @if( isset($ranks) )
-            <div class="form-group col location">
-                <span class="icon flaticon-stocks-graphic-on-laptop-monitor"></span>
-                <select name="rank_id" class="form-select chosen-select">
-                    <option value="">Tất cả cấp bậc</option>
-                    @foreach ($ranks as $rank)
-                        <option @selected( $rank->id == request()->rank_id ) value="{{ $rank->id }}">{{ $rank->name }}</option>
-                    @endforeach
-                </select>
-            </div>
+            @if (isset($ranks))
+                <div class="form-group col location">
+                    <span class="icon flaticon-stocks-graphic-on-laptop-monitor"></span>
+                    <select name="rank_id" class="form-select chosen-select">
+                        <option value="">Tất cả cấp bậc</option>
+                        @foreach ($ranks as $rank)
+                            <option @selected($rank->id == request()->rank_id) value="{{ $rank->id }}">{{ $rank->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
             @endif
-            @if( isset($wages) )
-            <div class="form-group col location">
-                <span class="icon flaticon-money"></span>
-                <select name="wage_id" class="form-select chosen-select">
-                    <option value="">Tất cả mức lương</option>
-                    @foreach ($wages as $wage)
-                        <option @selected( $wage->id == request()->wage_id )  value="{{ $wage->id }}">{{ $wage->name }}</option>
-                    @endforeach
-                </select>
-            </div>
+            @if (isset($wages))
+                <div class="form-group col location">
+                    <span class="icon flaticon-money"></span>
+                    <select name="wage_id" class="form-select chosen-select">
+                        <option value="">Tất cả mức lương</option>
+                        @foreach ($wages as $wage)
+                            <option @selected($wage->id == request()->wage_id) value="{{ $wage->id }}">{{ $wage->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
             @endif
-            @if( isset($careers) )
-            <div class="form-group col location">
-                <span class="icon flaticon-target"></span>
-                <select name="career_id" class="form-select chosen-select">
-                    <option value="">Tất cả ngành nghề</option>
-                    @foreach ($careers as $career)
-                        <option @selected( $career->id == request()->career_id ) value="{{ $career->id }}">{{ $career->name }}</option>
-                    @endforeach
-                </select>
-            </div>
+            @if (isset($careers))
+                <div class="form-group col location">
+                    <span class="icon flaticon-target"></span>
+                    <select name="career_id" class="form-select chosen-select">
+                        <option value="">Tất cả ngành nghề</option>
+                        @foreach ($careers as $career)
+                            <option @selected($career->id == request()->career_id) value="{{ $career->id }}">{{ $career->name }}
+                            </option>
+                        @endforeach
+                    </select>
+                </div>
             @endif
             <!-- Form Group -->
             <div class="form-group col btn-box">
@@ -61,33 +67,34 @@
             </div>
             <div id="advanceSearch" style="display:none" class="form-group col-lg-12">
                 <div class="row">
-                    @if( isset($degrees) )
-                    <div class="form-group col location">
-                        <span class="icon flaticon-stocks-graphic-on-laptop-monitor"></span>
-                        <select name="degree_id" class="form-select chosen-select">
-                            <option value="">Tất cả trình độ</option>
-                            @foreach ($degrees as $degree)
-                                <option @selected( $degree->id == request()->degree_id ) value="{{ $degree->id }}">{{ $degree->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
+                    @if (isset($degrees))
+                        <div class="form-group col location">
+                            <span class="icon flaticon-stocks-graphic-on-laptop-monitor"></span>
+                            <select name="degree_id" class="form-select chosen-select">
+                                <option value="">Tất cả trình độ</option>
+                                @foreach ($degrees as $degree)
+                                    <option @selected($degree->id == request()->degree_id) value="{{ $degree->id }}">
+                                        {{ $degree->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     @endif
-                    @if( isset($formworks) )
-                    <div class="form-group col location">
-                        <span class="icon flaticon-stocks-graphic-on-laptop-monitor"></span>
-                        <select name="formwork_id" class="form-select chosen-select">
-                            <option value="">Tất cả hình thức</option>
-                            @foreach ($formworks as $formwork)
-                                <option @selected( $formwork->id == request()->formwork_id ) value="{{ $formwork->id }}">{{ $formwork->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
+                    @if (isset($formworks))
+                        <div class="form-group col location">
+                            <span class="icon flaticon-stocks-graphic-on-laptop-monitor"></span>
+                            <select name="formwork_id" class="form-select chosen-select">
+                                <option value="">Tất cả hình thức</option>
+                                @foreach ($formworks as $formwork)
+                                    <option @selected($formwork->id == request()->formwork_id) value="{{ $formwork->id }}">
+                                        {{ $formwork->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     @endif
-                    
+
                 </div>
             </div>
         </div>
     </form>
 </div>
 @include('website.homes.includes.hero-banner')
-
