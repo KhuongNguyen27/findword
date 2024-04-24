2ae032ed1baf399c4f10e76ac38da4875fa88f68
Minamino9795 <minamino9795@gmail.com>
2024-04-24 16:26:08 +0700

    commit fix field transaction field

--- Modules/Transaction/resources/views/create.blade.php
+++ Modules/Transaction/resources/views/create.blade.php
@@ -13,7 +13,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="mb-4">
-                                    <label for="amount">Số tiền</label>
+                                    <label for="amount">Số tiền <span style="color: red">*</span></label>
                                     <input type="number" class="form-control" name="amount" id="amount" min="1000"
                                         max="100000000" placeholder="Nhập vào số tiền">
                                     @error('amount')
@@ -25,7 +25,8 @@
                                     <input type="text" class="form-control" value="FW{{ Auth::id() }}" disabled>
                                 </div>
                                 <div class="mb-4">
-                                    <button type="button" id="openModalBtn" class="form-control btn btn-primary">Thực hiện thanh toán</button> <!-- Nút mở modal -->
+                                    <button type="button" id="openModalBtn" class="form-control btn btn-primary">Thực hiện
+                                        thanh toán</button> <!-- Nút mở modal -->
                                 </div>
                             </div>
                         </div>
@@ -37,23 +38,43 @@
                     <span class="close">&times;</span>
                     <div class="card">
                         <div class="card-body">
-                            <div class="mb-4" style="text-align: center;">
-                                <h2>Thông tin tài khoản giao dịch</h2>
-                            </div>
-                            <div class="mb-4">
-                                <label for="amount">Số tài khoản</label>
-                                <input type="number" class="form-control" value="0123456789" disabled>
-                            </div>
-                            <div class="mb-4">
-                                <label for="amount">Tên ngân hàng</label>
-                                <input type="text" class="form-control" value="Vietcombank" disabled>
-                            </div>
-                            <div class="mb-4">
-                                <label for="transaction_code">Nội dung chuyển khoản</label>
-                                <input type="text" class="form-control" value="FW{{ Auth::id() }}" disabled>
-                            </div>
-                            <div class="mb-4">
-                                <p>Nếu bạn đã thực hiện giao dịch vui lòng chọn Xác nhận thanh toán.</p>
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="mb-4" style="text-align: center;">
+                                        <h2>Thông tin tài khoản giao dịch</h2>
+                                    </div>
+                                    <div class="mb-2">
+                                        <label for="amount">Số tài khoản</label>
+                                        <input type="number" class="form-control" value="3301729747" disabled>
+                                    </div>
+                                    <div class="mb-2">
+                                        <label for="amount">Tên ngân hàng</label>
+                                        <input type="text" class="form-control" value="Techcombank" disabled>
+                                    </div>
+                                    <div class="mb-2">
+                                        <label for="amount">Tên công ty</label>
+                                        <input type="text" class="form-control"
+                                            value="Công ty Cổ phần Cung ứng Nguồn nhân lực HR" disabled>
+                                    </div>
+                                    <div class="mb-2">
+                                        <label for="transaction_code">Nội dung chuyển khoản</label>
+                                        <input type="text" class="form-control" value="FW{{ Auth::id() }}" disabled>
+                                    </div>
+                                    <div class="mb-2">
+                                        <label for="amount">Số tiền nạp vào tài khoản</label>
+                                        <input type="number" class="form-control" name="amount" disabled>
+                                    </div>
+                                    <div class="mb-2">
+                                        <p style="color: red; text-align: center;">Vui lòng chỉ "Xác nhận thanh toán" khi đã
+                                        </p>
+                                        <p style="color: red; text-align: center;">HOÀN TẤT CHUYỂN KHOẢN THANH TOÁN. </p>
+                                    </div>
+                                </div>
+                                <div class="col-md-4" style="text-align: center;">
+                                    <h3>Quét mã QR để thanh toán</h3>
+                                    <img src="{{ asset('website-assets/images/qrcode/qrcode1.png') }}"
+                                        alt="Mã QR để thanh toán" class="img-fluid">
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -68,6 +89,9 @@
 
                 openModalBtn.addEventListener("click", function(event) {
                     event.preventDefault();
+                    var amount = document.getElementById("amount").value;
+                    var modalAmountField = document.querySelector("#paymentConfirmationModal input[name='amount']");
+                    modalAmountField.value = amount;
                     modal.style.display = "flex";
                 });
                 closeModal.addEventListener("click", function() {
