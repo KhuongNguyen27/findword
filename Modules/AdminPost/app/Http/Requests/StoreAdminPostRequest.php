096d606a8ac4182afce74c43351b7fee75e676b4
Nguyễn Tài Tâm <taitam8590@gmail.com>
2024-04-27 11:06:06 +0700

    fix max

--- Modules/AdminPost/app/Http/Requests/StoreAdminPostRequest.php
+++ Modules/AdminPost/app/Http/Requests/StoreAdminPostRequest.php
@@ -14,11 +14,11 @@ class StoreAdminPostRequest extends FormRequest
     {
         $rules = [
             'name' => 'required|max:255',
-            'description'=>'required|max:1000'
+            'description'=>'required'
         ];
         if ($this->isMethod('PUT') || $this->isMethod('PATCH')) {
             $rules['name'] = 'required|max:255';
-            $rules['description'] = 'required|max:1000';
+            $rules['description'] = 'required';
         }
         return $rules;
     }
