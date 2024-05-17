ed680bed625bdaba34b69cd6658ecd6283338318
hieutran01 <trandinhhieu19012002@gmail.com>
2024-05-17 18:58:48 +0700

    upload file

--- Modules/Employee/resources/views/profile/index.blade.php
+++ Modules/Employee/resources/views/profile/index.blade.php
@@ -214,45 +214,113 @@
                                         <p style="color:red">{{ $errors->first('about') }}</p>
                                         @endif
                                     </div>
+                                    <style>
+                                    .uploadButton-business .uploadButton-business-button {
+                                        display: flex;
+                                        align-items: center;
+                                        justify-content: center;
+                                        flex-direction: column;
+                                        cursor: pointer;
+                                        height: 120px;
+                                        width: 200px;
+                                        border-radius: 5px;
+                                        transition: 0.3s;
+                                        margin: 0;
+                                        color: #1b2032;
+                                        font-size: 16px;
+                                        border: 2px dashed #ced4e1;
+                                    }
 
+                                    .uploadButton-business .uploadButton-business-input {
+                                        opacity: 0;
+                                        position: absolute;
+                                        overflow: hidden;
+                                        z-index: -1;
+                                        pointer-events: none;
+                                        height: 0;
+                                        width: 0;
+                                        display: none;
+                                    }
+
+                                    .uploadButton-business .uploadButton-business-button:before {
+                                        font-family: "Flaticon";
+                                        content: "\f15c";
+                                        color: #9fa9b8;
+                                        font-size: 20px;
+                                        transition: 0.3s;
+                                    }
+
+                                    .uploadButton-business .uploadButton-business-file-name {
+                                        align-items: center;
+                                        padding: 0 10px;
+                                        padding-left: 18px;
+                                        min-height: 36px;
+                                        top: 1px;
+                                        position: relative;
+                                        color: #1b2032;
+                                        background-color: transparent;
+                                        overflow: hidden;
+                                        line-height: 22px;
+                                    }
+
+                                    .new-business-preview {
+                                        width: 100px;
+                                        height: 100px;
+                                        object-fit: contain;
+                                        /* margin-left: 230px; */
+                                    }
+
+                                    .new-business-preview {
+                                        display: inline-block;
+                                        /* width: 50%; */
+                                        /* height: 50%; */
+                                        text-align: center;
+                                        /* object-fit: contain; */
+                                    }
+
+                                    img {
+                                        display: inline-block;
+                                        width: 100%;
+                                        height: 100%;
+                                        text-align: center;
+                                        object-fit: contain;
+                                    }
+
+                                    .uploading-outer-business {
+                                        position: relative;
+                                        display: flex;
+                                        width: 100%;
+                                        padding-bottom: 30px;
+                                        border-bottom: 1px solid #f1f3f7;
+                                        margin-bottom: 30px;
+                                        align-items: center;
+                                    }
+
+                                    .uploading-outer-business .uploadButton-business {
+                                        position: relative;
+                                        display: flex;
+                                        justify-content: center;
+                                        align-items: center;
+                                    }
+                                    </style>
                                     <span><strong>{{ __('image_business_license') }}</strong></span>
-                                    <div class="uploading-outer">
-                                        <div class="uploadButton">
-                                            <input class="uploadButton-input" type="file" name="image_business_license"
-                                                accept="image/*, application/pdf" id="upload-business-license" multiple
+                                    <div class="uploading-outer-business">
+                                        <div class="uploadButton-business">
+                                            <input class="uploadButton-business-input" type="file"
+                                                name="image_business_license" accept="image/*, application/pdf"
+                                                id="upload-business-license" multiple
                                                 {{ (isset($user_employee) && $user_employee->user->verify == 1) ? 'disabled' : '' }}>
-                                            <label class="uploadButton-button ripple-effect"
-                                                for="upload-business-license">{{ __('image') }}</label>
-                                            <span class="uploadButton-file-name"></span>
+                                            <label class="uploadButton-business-button ripple-effect"
+                                                for="upload-business">{{ __('image') }}</label>
+                                            <span class="uploadButton-business-file-name"></span>
                                         </div>
-                                        <div class="new-image-preview" style="margin-left:0px">
+                                        <div class="new-business-preview" style="margin-left:0px">
                                             <?php if (isset($user_employee->image_business_license)): ?>
                                             <img src="<?php echo asset($user_employee->image_business_license); ?>"
                                                 alt="Preview Image">
                                             <?php endif; ?>
                                         </div>
                                     </div>
-
-                                    <span><strong>{{ __('logo_upload') }}</strong></span>
-                                <div class="uploading-outer">
-                                    <div class="uploadButton">
-                                        <input class="uploadButton-input" type="file" name="image"
-                                            accept="image/*, application/pdf" id="upload" multiple
-                                            {{ (isset($user_employee) && $user_employee->user->verify == 1) ? 'disabled' : '' }}>
-                                        <label class="uploadButton-button ripple-effect"
-                                            for="upload">{{ __('logo_browse') }}</label>
-                                        <span class="uploadButton-file-name"></span>
-                                    </div>
-                                    <div class="new-image-preview" style="margin-left:0px">
-                                        <?php if (isset($user_employee->image)):?>
-                                        <img src="<?php echo asset($user_employee->image); ?>" alt="Preview Image">
-                                        <?php endif; ?>
-                                    </div>
-                                </div>
-
-
-
-
                                     <span><strong> {{ __('background') }} </strong></span>
                                     <div class="uploading-outer-background">
                                         <div class="uploadButton-background">
@@ -302,18 +370,20 @@ $(document).ready(function() {
         }
     });
 });
+
 $(document).ready(function() {
     $('#upload-business-license').on('change', function() {
-        $('.new-image-preview').hide();
+        $('.new-business-preview').hide();
         var fileInput = $(this)[0];
         var file = fileInput.files[0];
         if (file && file.type.startsWith('image/')) {
-            var imageUrl = URL.createObjectURL(file);
-            $('.uploadButton-file-name').html('<img src="' + imageUrl +
-                '" alt="Preview Image" style="max-width: 150px; max-height: 120px;">');
+            var businessUrl = URL.createObjectURL(file);
+            $('.uploadButton-business-file-name').html('<img src="' + businessUrlUrl +
+                '" alt="Preview Background" style="max-width: 150px; max-height: 120px;">');
         }
     });
-});
+})
+
 $(document).ready(function() {
     $('#upload-background').on('change', function() {
         $('.new-background-preview').hide();
