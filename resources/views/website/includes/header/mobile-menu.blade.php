1e652f4b3975d13a91161bb0195377b4c3df0f1d
hieutran01 <trandinhhieu19012002@gmail.com>
2024-06-05 22:39:07 +0700

    fix view mobile

--- resources/views/website/includes/header/mobile-menu.blade.php
+++ resources/views/website/includes/header/mobile-menu.blade.php
@@ -4,11 +4,23 @@
 
     <!--Nav Box-->
     <div class="nav-outer clearfix">
-
         <div class="outer-box">
             <!-- Login/Register -->
             <div class="login-box">
-                <a onclick="window.location.href='{{ route('staff.register') }}'" href="{{ route('staff.register') }}" class="call-modal"><span class="icon-user"></span></a>
+                @if(Auth::check())
+                    @php
+                        $userType = Auth::user()->type; // assuming 'type' is the field that stores the user type
+                    @endphp
+
+                    @if($userType == 'staff')
+                        <a onclick="window.location.href='{{ route('staff.register') }}'" href="{{ route('staff.register') }}" class="call-modal"><span class="icon-user"></span></a>
+                    @elseif($userType == 'employee')
+                        <a onclick="window.location.href='{{ route('employee.register') }}'" href="{{ route('employee.register') }}" class="call-modal"><span class="icon-user"></span></a>
+                    @endif
+                @else
+                    <!-- If the user is not logged in, you can redirect them to a login page or show a generic link -->
+                    <a onclick="window.location.href='{{ route('login') }}'" href="{{ route('login') }}" class="call-modal"><span class="icon-user"></span></a>
+                @endif
             </div>
 
             <a href="#nav-mobile" class="mobile-nav-toggler"><span class="flaticon-menu-1"></span></a>
