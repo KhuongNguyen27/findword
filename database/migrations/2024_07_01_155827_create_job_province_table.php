96282fdd7e8e1860e960211a4b460cefdd36ce88
TelMerLin <tamnguyen01>
2024-07-01 17:38:37 +0700

    add nullable to migration

--- database/migrations/2024_07_01_155827_create_job_province_table.php
+++ database/migrations/2024_07_01_155827_create_job_province_table.php
@@ -11,8 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('jobs', function (Blueprint $table) {
-            $table->integer('top_position')->nullable();
+        Schema::table('job_province', function (Blueprint $table) {
+            $table->unsignedBigInteger('country_id')->nullable();
+     
         });
     }
 
@@ -21,8 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('jobs', function (Blueprint $table) {
-            $table->integer('top_position');
-        });
+        Schema::dropIfExists('job_province');
     }
 };
