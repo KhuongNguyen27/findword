d97c5949dc352f4d368963c5b354438474c0e0ad
hoanglongtpt <hoanglongtpt1999@gmail.com>
2024-08-28 16:04:10 +0700

    done seo trang Home

---
+++ config/seo.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'keywords' => ['cv', 'curriculum', 'vitae', 'resume', 'tao cv', 'viet cv', 'viet cv', 'truc tuyen', 'cv online', 'cv an tuong', 'mau cv dep'],
+    'title' => 'Tìm Việc Siêu Nhanh - CV xịn việc làm chất - Tạo CV & Tìm việc miễn phí',
+    'description' => 'Tạo CV online & tìm việc làm miễn phí trên timviecsieunhanh.vn ngay hôm nay để tiếp cận 30.000+ cơ hội việc làm được kết nối thành công trên timviecsieunhanh.vn mỗi ngày.',
+    'canonical' => 'https://timviecsieunhanh.vn/',
+];
